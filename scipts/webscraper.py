e45bdade4435d9287f5b51fffcab107968e86005
Marko <marko.cuk.888@gmail.com>
2023-10-29 17:28:07 +0100

    add proper error handling

--- scipts/webscraper.py
+++ scipts/webscraper.py
@@ -8,14 +8,20 @@ parsed_url = urlparse(url)
 if not parsed_url.scheme:
     url = "https://" + url
 
-response = requests.get(url)
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    a_tags = soup.find_all('a')
-    
-    for tag in a_tags:
-        print(f'Text: {tag.get_text()}')
-        print(f'Href: {tag.get("href")}')
-else:
-    print('Failed to retrieve the web page.')
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        a_tags = soup.find_all('a')
+        
+        for tag in a_tags:
+            print(f'Text: {tag.get_text()}')
+            print(f'Href: {tag.get("href")} \n')
+    else:
+        print(f'Failed to retrieve the web page. Status Code: {response.status_code}')
+except requests.exceptions.RequestException as e:
+    print(f'An error occurred while making the request (possible thaht the Host is unreachable. Did you use format : www.xyz.com ?): {e}')
+except Exception as e:
+    print(f'An unexpected error occurred: {e}')
